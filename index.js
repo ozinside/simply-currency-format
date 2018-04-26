15651be9d556d645ed3400fc3356f6d32d0f2a6b
Young Jung <young_dev@ozinside.com>
2018-04-26 13:59:53 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const defaultLocale = 'en-US';
-const defaultOptions = {
-  style: 'currency',
-  currency: 'USD'
+module.exports = function simplyFormatCurrency (numVal) {
+  return isNaN(parseFloat(numVal)) ? '$0.00' : parseFloat(numVal).toLocaleString('en-AU', {style: 'currency', currency: 'USD'});
 };
 
-function simplyFormatCurrency (numVal, locale, opts) {
-  opts = Object.assign({}, defaultOptions, opts);  
-  return isNaN(parseFloat(numVal)) ? '$0.00' : parseFloat(numVal).toLocaleString(locale, opts);
-}
-
-function simplyFormatCurrency (numVal) {
-  return isNaN(parseFloat(numVal)) ? '$0.00' : parseFloat(numVal).toLocaleString('en-AU', {style: 'currency', currency: 'USD'});
-}
-
-module.exports = simplyFormatCurrency;
+module.exports = function simplyFormatCurrencyNum (numVal) {
+  return isNaN(parseFloat(numVal)) ? '0.00' : parseFloat(numVal).toFixed(2);
+};
