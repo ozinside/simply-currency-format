5950fbe456a243b50354908be7ee6c8848786bad
Young Jung <young_dev@ozinside.com>
2018-04-26 14:03:14 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 module.exports = function simplyFormatCurrency (numVal) {
   return isNaN(parseFloat(numVal)) ? '$0.00' : parseFloat(numVal).toLocaleString('en-AU', {style: 'currency', currency: 'USD'});
 };
-
-module.exports = function simplyFormatCurrencyNum (numVal) {
-  return isNaN(parseFloat(numVal)) ? '0.00' : parseFloat(numVal).toFixed(2);
-};
